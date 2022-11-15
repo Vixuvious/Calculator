be74078298ac6122c17ea5cb3954f92e9b0d1b7e
Vixuvious <118066058+Vixuvious@users.noreply.github.com>
2022-11-15 09:08:15 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 "use strict";
 
-let buttonA = document.getElementById('button1');
-buttonA.addEventListener('click', clicker);
+let button = document.getElementById('button');
+button.addEventListener('click', clicker);
 
 function clicker(e) {
   console.log('button was clicked');
-  let numberA = document.getElementById("number1");
-  numberA.innerText = Number(numberA.innerText) + 1;
+  let number = document.getElementById("number");
+  number.innerText = Number(number.innerText) + 1;
 }
 
-let buttonB = document.getElementById('button2');
-buttonB.addEventListener('click', clicker2);
+// let buttonB = document.getElementById('button2');
+// buttonB.addEventListener('click', clicker2);
 
-function clicker2(e) {
-  console.log('button 2 was clicked');
-  let numberA = document.getElementById("number1");
-  numberA.innerText = Number(numberA.innerText) * 2;
-}
+// function clicker2(e) {
+//   console.log('button 2 was clicked');
+//   let numberA = document.getElementById("number1");
+//   numberA.innerText = Number(numberA.innerText) * 2;
+// }
