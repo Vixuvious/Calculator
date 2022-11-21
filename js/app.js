9a5f429781124f53b5fcd119fe2b776d34c8973e
Vixuvious <118066058+Vixuvious@users.noreply.github.com>
2022-11-21 12:10:47 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,69 @@
 "use strict";
 
-let buttonA = document.getElementById('button1');
-buttonA.addEventListener('click', clicker);
+let numberHold = 1
 
-function clicker(e) {
-  console.log('button was clicked');
+//Add Number on Hold
+let buttonAdd = document.getElementById('buttonAdd');
+buttonAdd.addEventListener('click', clickerAdd);
+
+function clickerAdd(e) {
+  console.log('buttonAdd was clicked');
   let numberA = document.getElementById("number1");
-  numberA.innerText = Number(numberA.innerText) + 1;
+  numberA.innerText = Number(numberA.innerText) + numberHold;
 }
 
-// let buttonB = document.getElementById('button2');
-// buttonB.addEventListener('click', clicker2);
+//Multiply number on hold
+let buttonB = document.getElementById('buttonMult');
+buttonB.addEventListener('click', clickerMult);
 
-// function clicker2(e) {
-//   console.log('button 2 was clicked');
-//   let numberA = document.getElementById("number1");
-//   numberA.innerText = Number(numberA.innerText) * 2;
-// }
+function clickerMult(e) {
+  console.log('button Mult was clicked');
+  let numberA = document.getElementById("number1");
+  numberA.innerText = Number(numberA.innerText) * numberHold;
+}
+
+//Set the number to 0
+let buttonClear = document.getElementById('buttonClear');
+buttonClear.addEventListener('click', clickerClear);
+
+function clickerClear(e) {
+  console.log('button Clear was clicked');
+  let numberA = document.getElementById("number1");
+  numberA.innerText = Number(numberA.innerText) * 0;
+}
+
+//Button 1
+let button1 = document.getElementById('buttonOne');
+buttonOne.addEventListener('click', clickerNum1);
+
+function clickerNum1(e) {
+  console.log('button 1 was clicked');
+  numberHold = 1
+}
+
+//Button 2
+let button2 = document.getElementById('buttonTwo');
+buttonTwo.addEventListener('click', clickerNum2);
+
+function clickerNum2(e) {
+  console.log('button 2 was clicked');
+  numberHold = 2
+}
+
+//Button 3
+let button3 = document.getElementById('buttonThree');
+button3.addEventListener('click', clickerNum3);
+
+function clickerNum3(e) {
+  console.log('button 3 was clicked');
+  numberHold = 3
+}
+
+//Button 4
+let button4 = document.getElementById('buttonFour');
+button4.addEventListener('click', clickerNum4);
+
+function clickerNum4(e) {
+  console.log('button 4 was clicked');
+  numberHold = 4
+}
